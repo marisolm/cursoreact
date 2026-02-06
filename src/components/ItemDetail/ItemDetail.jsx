325a03b6052730567bc1564b89ec9a13a24e9691
MARECOS <msmareco@ab-inbev.com>
2026-02-06 09:52:17 -0300

    correcciones en visualización items por ID

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,12 +2,14 @@ import "./ItemDetail.css";
 
 const ItemDetail = ({ product }) => {
     return (
-        <div className="itemDetail">
+        <div className="item-detail">
             <img src={product.image}/>
             <p>{product.name}</p>
             <p>{product.description}</p>
             <p>Precio: ${product.price}</p>        
+            <button className="item-detail-button">Agregar al carrito</button>
         </div>
+
     );
 };
 export default ItemDetail;
